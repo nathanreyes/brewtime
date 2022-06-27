beb5f4d50bf98e3d0c5a92f305ccf51440bcfa32
Nathan Reyes <nathanreyes.me@gmail.com>
2022-06-27 13:27:55 -0500

    Buttons

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 defineProps<{
   active?: boolean;
+  isLarge?: boolean;
 }>();
 </script>
 <template>
   <button
-    class="flex justify-center items-center w-full h-16 space-x-4 border-gray-700"
+    class="flex justify-center items-center w-full space-x-3 border-gray-700"
     :class="[
       active
         ? 'bg-black hover:bg-gray-900 text-white dark:bg-gray-600 dark:hover:bg-gray-700'
         : 'hover:bg-gray-50 dark:hover:bg-gray-700',
+      isLarge ? 'h-16' : 'h-10',
     ]"
   >
     <slot />
---
+++ src/components/BaseButtonGroup.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue';
+
+interface Button {
+  label: string;
+  icon?: any;
+  value: any;
+}
+defineProps<{
+  buttons: Button[];
+  modelValue: any;
+}>();
+defineEmits();
+</script>
+<template>
+  <div class="flex border border-black divide-x divide-black">
+    <BaseButton
+      v-for="button in buttons"
+      :key="button.label"
+      :active="button.value === modelValue"
+      @click="$emit('update:modelValue', button.value)"
+    >
+      <component :is="button.icon" />
+      <span>{{ button.label }}</span>
+    </BaseButton>
+  </div>
+</template>

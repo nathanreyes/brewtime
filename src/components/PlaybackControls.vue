7e73eb25720a87762833cf0e05d0ff3952da32e4
Nathan Reyes <nathanreyes.me@gmail.com>
2022-07-08 16:31:44 -0500

    Update PlaybackControls.vue

--- src/components/PlaybackControls.vue
+++ src/components/PlaybackControls.vue
@@ -31,16 +31,16 @@ const sizeClass = computed(() => (props.isLg ? 'w-6 h-6' : 'w-5 h-5'));
   <div>
     <ProgressBar v-model="progress" />
     <div class="flex justify-between items-center" :class="[isLg ? 'mt-4' : 'mt-3']">
-      <div class="flex justify-start items-center w-1/3 space-x-4">
+      <div class="flex justify-start items-center w-2/5 space-x-4">
         <button @click="$emit('skip-back')"><IconSkipBack :class="sizeClass" /></button>
         <button @click="$emit('skip-forward')"><IconSkipForward :class="sizeClass" /></button>
         <button @click="$emit('reset')"><IconRefreshCw :class="sizeClass" /></button>
       </div>
-      <div class="flex justify-center items-center w-1/3">
+      <div class="flex justify-center items-center w-1/5">
         <button v-if="running" @click="$emit('pause')"><IconPause :class="sizeClass" /></button>
         <button v-else @click="$emit('play')"><IconPlay :class="sizeClass" /></button>
       </div>
-      <div class="flex justify-end items-center w-1/3">
+      <div class="flex justify-end items-center w-2/5">
         <p class="text-center select-none" :class="[isLg ? '' : 'text-sm']">{{ durationLabel }} / {{ totalLabel }}</p>
       </div>
     </div>

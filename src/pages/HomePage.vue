bdeecf3453f03e12b538eb0d20c19346f91de672
Nathan Reyes <nathanreyes.me@gmail.com>
2022-07-15 11:29:05 -0500

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,8 +4,10 @@
     <AppHeader title="Welcome to GifMyCoffee!" hide-menu />
     <main class="flex-grow relative">
       <div class="absolute inset-0 flex flex-col w-full sm:max-w-xl mx-auto">
-        <p class="mt-4">GifMyCoffee is a free coffee timer app that helps people brew coffee with gifs.</p>
-        <p class="my-4">That's it! Select a brew method to get started.</p>
+        <div class="px-4 sm:px-0">
+          <p class="mt-4">GifMyCoffee is a free coffee timer app that helps people brew coffee with gifs.</p>
+          <p class="my-4">That's it! Select a brew method to get started.</p>
+        </div>
         <BrewGrid />
       </div>
     </main>

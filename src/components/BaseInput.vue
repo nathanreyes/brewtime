7574b22f04b96184ae579d79746a1c88fd7e79ca
Nathan Reyes <nathanreyes.me@gmail.com>
2022-07-16 11:09:05 -0500

    Update BaseInput.vue

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -2,13 +2,16 @@
 defineProps<{
   modelValue: any;
 }>();
-defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue']);
+function onInput(evt: Event) {
+  emit('update:modelValue', (evt.target as HTMLInputElement).value);
+}
 </script>
 
 <template>
   <input
     :value="modelValue"
     class="block px-3 bg-white dark:bg-black border border-black dark:border-gray-500 text-black dark:text-white rounded"
-    @input="$emit('update:modelValue', $event.target.value)"
+    @input="onInput"
   />
 </template>

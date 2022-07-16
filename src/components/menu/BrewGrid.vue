980e31a4b0b6eddfcf9cd6c9ad656f6fe7beb6c1
Nathan Reyes <nathanreyes.me@gmail.com>
2022-07-16 11:35:15 -0500

    Update BrewGrid.vue

--- src/components/menu/BrewGrid.vue
+++ src/components/menu/BrewGrid.vue
@@ -6,7 +6,7 @@ const selectedBrew = ref<any>(null);
 </script>
 <template>
   <div v-if="!selectedBrew">
-    <div class="grid grid-flow-col auto-cols-fr gap-4 m-4 mt-2 sm:m-0">
+    <div class="grid grid-cols-2 sm:grid-cols-3 auto-cols-fr gap-4 m-4 mt-2 sm:m-0">
       <BrewGridItem v-for="brew in brews" :key="brew.name" :brew="brew" />
     </div>
   </div>
